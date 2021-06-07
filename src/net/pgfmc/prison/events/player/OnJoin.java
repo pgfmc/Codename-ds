1c454e41dd021be7d1c6169c534dba4d4d5dbcb4
bk <bk123yt@gmail.com>
2021-06-07 02:43:45 -0500

    newPlayerItems

--- src/net/pgfmc/prison/events/player/OnJoin.java
+++ src/net/pgfmc/prison/events/player/OnJoin.java
@@ -1,11 +1,15 @@
 package net.pgfmc.prison.events.player;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
 import org.bukkit.Material;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
@@ -15,12 +19,13 @@ import net.pgfmc.prison.Main;
 
 public class OnJoin implements Listener {
 	
-	private ItemStack[] newPlayerItems = { new ItemStack(Material.IRON_PICKAXE), new ItemStack(Material.COOKED_BEEF) }; // Items new players should receive
+	private ItemStack[] newPlayerItems = { new ItemStack(Material.IRON_PICKAXE), new ItemStack(Material.COOKED_BEEF, 16) }; // Items new players should receive
 	
 	@EventHandler
 	public void onJoin(PlayerChangedWorldEvent e)
 	{
-		if (e.getPlayer().getWorld().getName() != "Prison") { return; } // Return if the world isn't Prison
+		Player p = e.getPlayer();
+		if (p.getWorld().getName() != "Prison") { return; } // Return if the world isn't Prison
 		
 		File file = new File(Main.plugin.getDataFolder() + File.separator + "logs" + File.separator + "join.yml"); // Creates a File object
 		FileConfiguration db = YamlConfiguration.loadConfiguration(file); // Turns the File object into YAML and loads data
@@ -29,14 +34,34 @@ public class OnJoin implements Listener {
 		
 		try {
 			db.load(file); // loads file
-			if (db.get("uuids") == null)
+			if (db.get("uuids.") == null) // If the file is empty
 			{
-				newPlayerItems(e.getPlayer());
+				p.getInventory().addItem(newPlayerItems);
+				db.set("uuids.", p.getUniqueId().toString());
+				
+				return;
 			}
 			
-		} finally
-		{
+			@SuppressWarnings("unchecked")
+			List<String> uuids = (List<String>) db.get("uuids."); // Gets the list of players who have joined Prison
+			if (!uuids.contains(p.getUniqueId().toString())) // If the player has not previously joiend
+			{
+				p.getInventory().addItem(newPlayerItems);
+				db.set("uuids.", p.getUniqueId().toString());
+				return;
+			}
+			
 			return;
+			
+		} catch (FileNotFoundException except) {
+			// TODO Auto-generated catch block
+			except.printStackTrace();
+		} catch (IOException except) {
+			// TODO Auto-generated catch block
+			except.printStackTrace();
+		} catch (InvalidConfigurationException except) {
+			// TODO Auto-generated catch block
+			except.printStackTrace();
 		}
 				
 
